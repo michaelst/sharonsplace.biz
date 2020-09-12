3f77010265c6bea3286f89a7c32ea4e340322830
Michael St Clair <michaelst57@gmail.com>
2020-09-12 11:43:33 -0600

    setup services page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,6 +11,7 @@ module.exports = function(defaults) {
     }
   });
 
+  app.import('vendor/bootstrap.bundle.js');
   app.import('vendor/theme.js');
 
   // Use `app.import` to add additional libraries to the generated
